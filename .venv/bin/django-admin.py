a68f348b140f504a86f4fbed31abdbe60944cdbf
CoreyDeJong <dejongcorey@gmail.com>
2020-06-23 14:21:53 -0700

    delete venv

--- .venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/mnt/c/Users/dejon/Documents/Codefellows/401/labs/drf-api/.venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
